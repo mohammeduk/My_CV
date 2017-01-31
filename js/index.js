4f1295ff972eba2e6b190af570f2bdd6f9b4693c
Mohammad Usman Khan <mohammeduk@spartaglobal.co>
2017-01-31 23:23:20 +0000

    Completed UNRESPONSIVE design of CV section

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-// $(function() {
-// 	$('a[href*=#]').on('click', function(e) {
-// 		e.preventDefault();
-// 		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-// 	});
-// });
-document.querySelector('#para').scrollIntoView({
-  behavior: 'smooth'
+$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
 });
+
+// document.querySelector('#para').scrollIntoView({
+//   behavior: 'smooth'
+// });
